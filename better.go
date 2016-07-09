fa066aad5307d68be959b43524578af3cc736c8d
damouse <damouse007@gmail.com>
2016-07-09 15:10:54 -0500

    it may not be a memory issue, but not sure

--- better.go
+++ better.go
@@ -27,7 +27,6 @@ static void register_sig_handler() {
 import "C"
 
 import (
-	"encoding/json"
 	"fmt"
 	"sync"
 
@@ -35,9 +34,15 @@ import (
 	"github.com/sbinet/go-python"
 )
 
+var module *python.PyObject
+var fn *python.PyObject
+
 func Initialize() {
-	// C.pyinit(C.int(1))
-	// C.register_sig_handler()
+	gil := python.PyGILState_Ensure()
+	module = python.PyImport_ImportModule("adder")
+	fn = module.GetAttrString("run")
+	python.PyGILState_Release(gil)
+	checkPyErr()
 }
 
 // We can likely pool the pthreads, but that sounds like a problem for future mickey
@@ -45,47 +50,63 @@ func PyCall() string {
 	done := make(chan string)
 
 	t := pthread.Create(func() {
-		// C.register_sig_handler()
+		gil := python.PyGILState_Ensure()
 
-		// Lets try it with the gopython code...
-		gil := C.PyGILState_Ensure()
-		defer C.PyGILState_Release(gil)
-
-		m := python.PyImport_ImportModule("adder")
-		fn := m.GetAttrString("run")
+		// m := python.PyImport_ImportModule("adder")
+		// m.IncRef()
+		// fn := module.GetAttrString("run")
+		// fn.IncRef()
 
 		// Pack the arguments
-		args := python.PyTuple_New(2)
-		python.PyTuple_SET_ITEM(args, 0, python.PyString_FromString("Hello!"))
-		python.PyTuple_SET_ITEM(args, 1, python.PyInt_FromLong(1234))
+		// args := python.PyTuple_New(2)
+		// python.PyTuple_SET_ITEM(args, 0, python.PyString_FromString("Hello!"))
+		// python.PyTuple_SET_ITEM(args, 1, python.PyInt_FromLong(1234))
+
+		args := python.PyTuple_New(0)
+		args.IncRef()
 
 		ret := fn.CallObject(args)
-		done <- python.PyString_AsString(ret)
 
-		// cb(ret)
-		// done <- nil
+		defer ret.DecRef()
+		defer args.DecRef()
+
+		checkPyErr()
+		python.PyGILState_Release(gil)
+		checkPyErr()
+
+		done <- "Done"
+
+		// done <- python.PyString_AsString(ret)
 	})
 
 	defer t.Kill()
+	defer close(done)
 	return <-done
 }
 
+func checkPyErr() {
+	if python.PyErr_CheckSignals() {
+		python.PyErr_PrintEx(false)
+	}
+}
+
 func BetterTest() {
-	n := 100
+	n := 10
 	wg := sync.WaitGroup{}
 	wg.Add(n)
 
 	for i := 0; i < n; i++ {
 		go func(gid int) {
-			for j := 0; j < 100; j++ {
-				rs := PyCall()
+			for j := 0; j < 1000; j++ {
+				PyCall()
+				fmt.Printf("gorutine: %d iteration: %d\n", gid, j)
 
-				var retjson []interface{}
-				if _err := json.Unmarshal([]byte(rs), &retjson); _err != nil {
-					panic(fmt.Errorf("got invalid result from python function, `%v`\n", rs))
-				}
+				// var retjson []interface{}
+				// if _err := json.Unmarshal([]byte(rs), &retjson); _err != nil {
+				// 	panic(fmt.Errorf("got invalid result from python function, `%v`\n", rs))
+				// }
 
-				fmt.Printf("gorutine: %d iteration: %d pid: %f\n", gid, j, retjson[0])
+				// fmt.Printf("gorutine: %d iteration: %d pid: %f\n", gid, j, retjson[0])
 			}
 
 			wg.Done()
