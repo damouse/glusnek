5345fb299601d3d755f39e4c25498a44fd692a98
damouse <damouse007@gmail.com>
2016-07-09 14:37:23 -0500

    looks like it all works. yay!

--- better.go
+++ better.go
@@ -14,11 +14,12 @@ import (
 	"github.com/sbinet/go-python"
 )
 
-func create_thread(cb func(a *python.PyObject)) {
-	done := make(chan error)
+// We can likely pool the pthreads, but that sounds like a problem for future mickey
+func PyCall() string {
+	done := make(chan string)
 
-	var t pthread.Thread
-	t = pthread.Create(func() {
+	t := pthread.Create(func() {
+
 		// Lets try it with the gopython code...
 		gil := C.PyGILState_Ensure()
 		defer C.PyGILState_Release(gil)
@@ -32,31 +33,29 @@ func create_thread(cb func(a *python.PyObject)) {
 		python.PyTuple_SET_ITEM(args, 1, python.PyInt_FromLong(1234))
 
 		ret := fn.CallObject(args)
-		cb(ret)
+		done <- python.PyString_AsString(ret)
 
-		done <- nil
-		// t.Kill()
+		// cb(ret)
+		// done <- nil
 	})
 
-	<-done
-	t.Kill()
+	defer t.Kill()
+	return <-done
 }
 
 func BetterTest() {
-	for i := 0; i < 3; i++ {
-		go func() {
-			// for j := 0; j < 10; j++ {
-			create_thread(func(result *python.PyObject) {
-				rs := python.PyString_AsString(result)
+	for i := 0; i < 100; i++ {
+		go func(gid int) {
+			for j := 0; j < 100; j++ {
+				rs := PyCall()
 
 				var retjson []interface{}
 				if _err := json.Unmarshal([]byte(rs), &retjson); _err != nil {
 					panic(fmt.Errorf("got invalid result from python function, `%v`\n", rs))
 				}
 
-				fmt.Printf("gorutine: %d iteration: %d pid: %f\n", i, i, retjson[0])
-			})
-			// }
-		}()
+				fmt.Printf("gorutine: %d iteration: %d pid: %f\n", gid, j, retjson[0])
+			}
+		}(i)
 	}
 }
